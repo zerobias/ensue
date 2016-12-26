d0386be6672f2fb569ebf8a913aabbe868b55f9d
Dmitry Boldyrev <ribkatt@gmail.com>
2016-12-27 02:21:51 +0300

    Completly implemented new algorhythm

--- rollup.config.js
+++ rollup.config.js
@@ -4,30 +4,36 @@ import cleanup from 'rollup-plugin-cleanup'
 // import uglify from 'rollup-plugin-uglify'
 // import { minify } from 'uglify-js'
 
-let pkg = require('./package.json')
-let external = Object.keys(pkg.dependencies)
+const pkg = require('./package.json')
+const external = Object.keys(pkg.dependencies)
 
-const isUmd = process.env.BUILD === 'umd'
+// const isUmd = process.env.BUILD === 'umd'
 
 const builds = {
-  cmj : pkg.main,
-  es  : pkg.module,
-  umd : pkg.browser
+  cjs: pkg.main,
+  es : pkg.module,
+  umd: pkg.browser
 }
 
 const target = [{
-  dest: isUmd?builds.umd:builds.cmj,
-  format: isUmd?'umd':'cjs',
+  dest      : builds.cjs,
+  format    : 'cjs',
   moduleName: 'ensue',
-  sourceMap: true
+  sourceMap : true,
+  preferConst: true
+}, {
+  dest      : builds.umd,
+  format    : 'umd',
+  moduleName: 'ensue',
+  sourceMap : 'inline'
 }]
 const plugins = [
   cleanup(),
   nodeResolve({
-    jsnext: true,
-    main: false,
-    module: true,
-    skip: true,
+    jsnext        : true,
+    main          : false,
+    module        : true,
+    skip          : true,
     preferBuiltins: false
   }),
   commonjs({
@@ -35,11 +41,12 @@ const plugins = [
   })
 ]
 export default {
-  entry:'es/index.js',
+  entry  : 'es/index.js',
   globals: {
-    "ramda": "R"
+    'array-flatten': 'flatten'
   },
-  plugins: plugins,
-  external: external,
-  targets: target
+  preferConst: true,
+  plugins    : plugins,
+  external   : external,
+  targets    : target
 }
